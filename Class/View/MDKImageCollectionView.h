d43e23227820a4cc12432eebcb145fd93fa253b3
mikun <mikun@mikundeiMac.local>
2018-08-07 22:10:20 +0800

    no message

--- Class/View/MDKImageCollectionView.h
+++ Class/View/MDKImageCollectionView.h
@@ -6,7 +6,7 @@
 //  Copyright © 2018年 mdk. All rights reserved.
 //
 
-
+#import "MDKImageCollection-Swift.h"
 
 #pragma clang diagnostic push
 #pragma clang diagnostic ignored "-Wnullability-completeness"
